1ae8c02f788af63f92c5c656d844b4b37714819b
Nolan Shami <nolan.shami@stu.ocsb.ca>
2023-01-09 19:25:59 +0000

    Added code.py

---
+++ code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Nolan Shami
+# Created on: January 9th 2023
+# This CPT program is the same program
+# as the "Space Aliens" program on the pyBadge!
+
+import ugame
+import stage
+
+def game_scene():
+    # This function is the main game game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ =="__main__":
+    game_scene()
