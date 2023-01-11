0fafa5f17da5b7d5811f01c2eec14731e235f1d8
Nolan Shami <nolan.shami@stu.ocsb.ca>
2023-01-11 02:33:47 +0000

    Added code.py

--- code.py
+++ code.py
@@ -11,12 +11,25 @@ import stage
 def game_scene():
     # This function is the main game game_scene
 
-    print("\n\n\n") # 3 blank lines
-    print("Hello World!")
+    # Image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    # Set the background image to 0 in the image bank
+    # and size (10x8 tiles of size 16x16)
+    background=stage.Grid(image_bank_background, 10, 8)
+
+    # Create a stage for the background to show up on
+    # and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # Set the layers of all sprites, items show up in order
+    game.layers = [background]
+    # Render all sprites
+        # most likely you will only render the background once per game_scene
+    game.render_block()
 
     # repeat forever, game loop
     while True:
-        pass # just a placeholder for now
+        pass # for now it's just a placeholder
 
 if __name__ =="__main__":
     game_scene()
